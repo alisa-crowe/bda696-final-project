485c591c7373cfd3dd56bd4c032a5d3987791594
alisa-crowe <acrowe3069@sdsu.edu>
2025-12-09 09:44:57 -0800

    prepare scripts to create documents for rag

---
+++ chatbot/prepare-documents/prepare_team_docs.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+
+teams = pd.read_csv("combined-data/combined_team_and_player_data.csv")
+themes = pd.read_csv("success-factors/team_data_with_themes_final.csv")
